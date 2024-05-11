9ba22e02e0a7954a784d2240b44a8f23d179f42e
eivydasbalCommerce <eivydas.b@commercecore.com>
2024-05-11 16:23:02 +0300

    Created context for categories and products

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,21 @@ import type { AppProps } from 'next/app';
 import { ApolloProvider, ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import apolloClient from '../lib/apollo-client'; // Adjust the import path based on where your apollo-client.ts is located relative to _app.tsx
 import '../../styles/globals.css'; // Import global styles
+import '../../styles/main.css';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
-import { CategoryProvider } from '@/context/CategoryContext';
+import { ProductDataProvider } from '@/context/ProductDataContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
-      <CategoryProvider>
+      <ProductDataProvider>
         <Header />
         <div className="bg-white">
           <Component {...pageProps} />
         </div>
         <Footer />
-      </CategoryProvider>
+      </ProductDataProvider>
     </ApolloProvider>
   );
 }
