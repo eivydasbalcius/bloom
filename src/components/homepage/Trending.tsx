d8aa642eaebfbb4d7e08e9db7f8f224b6f703732
eivydasbalCommerce <eivydas.b@commercecore.com>
2024-05-16 04:26:11 +0300

    added href to /products page

--- src/components/homepage/Trending.tsx
+++ src/components/homepage/Trending.tsx
@@ -12,35 +12,35 @@ const Trending = () => {
 
   useEffect(() => {
     console.log('Trending Products:', trendingProducts);
-  }, [trendingProducts]); 
-    return (
-        <section aria-labelledby="trending-heading" className="bg-white">
-        <div className="py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-32">
-          <div className="flex items-center justify-between px-4 sm:px-6 lg:px-0">
-            <h2 id="trending-heading" className="text-2xl font-bold tracking-tight text-gray-900">
-              Trending products
-            </h2>
-            <a href="#" className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block">
-              See everything
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
-          </div>
+  }, [trendingProducts]);
+  return (
+    <section aria-labelledby="trending-heading" className="bg-white">
+      <div className="py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-32">
+        <div className="flex items-center justify-between px-4 sm:px-6 lg:px-0">
+          <h2 id="trending-heading" className="text-2xl font-bold tracking-tight text-gray-900">
+            Trending products
+          </h2>
+          <a href="/products" className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block">
+            See everything
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
 
-          <div className="relative mt-8">
-            <div className="relative w-full overflow-x-auto">
-              <ul
-                role="list"
-                className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0"
-              >
-                {trendingProducts.map((product) => (
-                  <li key={product.id} className="inline-flex w-64 flex-col text-center lg:w-auto">
-                    <Link href={`/products/${product.slug}`}>
+        <div className="relative mt-8">
+          <div className="relative w-full overflow-x-auto">
+            <ul
+              role="list"
+              className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0"
+            >
+              {trendingProducts.map((product) => (
+                <li key={product.id} className="inline-flex w-64 flex-col text-center lg:w-auto">
+                  <Link href={`/products/${product.slug}`}>
                     <div className="group relative">
                       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200">
-                      <img
-                        src={product.image.mediaItemUrl}
-                        alt={product.image.slug}
-                        className="h-full w-full object-cover object-center group-hover:opacity-75"
+                        <img
+                          src={product.image.mediaItemUrl}
+                          alt={product.image.slug}
+                          className="h-full w-full object-cover object-center group-hover:opacity-75"
                         />
                       </div>
                       <div className="mt-6">
@@ -54,23 +54,23 @@ const Trending = () => {
                         <p className="mt-1 text-gray-900">{product.price} €</p>
                       </div>
                     </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
-          <div className="mt-12 px-4 sm:hidden">
-            <a href="#" className="text-sm font-semibold text-indigo-600 hover:text-indigo-500">
-              See everything
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </section>
 
-);
+        <div className="mt-12 px-4 sm:hidden">
+          <a href="/products" className="text-sm font-semibold text-indigo-600 hover:text-indigo-500">
+            See everything
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+      </div>
+    </section>
+
+  );
 };
 
 export default Trending;
