6a185f952c7fce079164bf7dbebb24d96e98af8b
eivydasbalCommerce <eivydas.b@commercecore.com>
2024-05-20 18:53:58 +0300

    refactor: Add lazy loading for images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const { hostname } = require("os");
+
 module.exports = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/*",
+      },
+    ],
     domains: ["tailwindui.com"],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
