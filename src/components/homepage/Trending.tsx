c24618b25cc349ef4fcfaf8f1c01be620155946a
eivydasbalCommerce <eivydas.b@commercecore.com>
2024-05-20 18:08:03 +0300

    refactor: Update Trending component to use typed product objects

--- src/components/homepage/Trending.tsx
+++ src/components/homepage/Trending.tsx
@@ -1,10 +1,74 @@
-
 import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { useProductData } from "../../context/ProductDataContext";
 import { useRouter } from 'next/router';
 
-const Trending = () => {
+// Include the Product type definition
+interface MediaItem {
+  __typename: string;
+  slug: string;
+  mediaItemUrl: string;
+}
+
+interface ProductTag {
+  __typename: string;
+  name: string;
+  slug: string;
+}
+
+interface ProductCategory {
+  __typename: string;
+  id: string;
+  name: string;
+  slug: string;
+  parentId: string | null;
+  image: MediaItem | null;
+}
+
+interface TermNode {
+  __typename: string;
+  id: string;
+  name: string;
+  count: number;
+  slug: string;
+}
+
+interface Attribute {
+  __typename: string;
+  id: string;
+  attributeId: number;
+  name: string;
+  label: string;
+  options: string[];
+  terms: {
+    __typename: string;
+    nodes: TermNode[];
+  };
+}
+
+interface Product {
+  __typename: string;
+  id: string;
+  name: string;
+  description: string;
+  price: string;
+  slug: string;
+  image: MediaItem;
+  productTags: {
+    __typename: string;
+    nodes: ProductTag[];
+  };
+  productCategories: {
+    __typename: string;
+    nodes: ProductCategory[];
+  };
+  attributes: {
+    __typename: string;
+    nodes: Attribute[] | null;
+  };
+}
+
+const Trending: React.FC = () => {
   const { products } = useProductData();
   const router = useRouter();
 
@@ -13,13 +77,14 @@ const Trending = () => {
     router.push('/products');
   }
 
-  const trendingProducts = products.filter(product =>
+  const trendingProducts: Product[] = products.filter((product: Product) =>
     product?.productTags?.nodes.some(tag => tag.slug.toLowerCase() === 'trending')
   );
 
   useEffect(() => {
     console.log('Trending Products:', trendingProducts);
   }, [trendingProducts]);
+
   return (
     <section aria-labelledby="trending-heading" className="bg-white">
       <div className="py-16 sm:py-24 lg:mx-auto lg:max-w-7xl lg:px-8 lg:py-32">
@@ -39,7 +104,7 @@ const Trending = () => {
               role="list"
               className="mx-4 inline-flex space-x-8 sm:mx-6 lg:mx-0 lg:grid lg:grid-cols-4 lg:gap-x-8 lg:space-x-0"
             >
-              {trendingProducts?.map((product) => (
+              {trendingProducts?.map((product: Product) => (
                 <li key={product.id} className="inline-flex w-64 flex-col text-center lg:w-auto">
                   <Link href={`/products/${product.slug}`}>
                     <div className="group relative">
@@ -51,9 +116,9 @@ const Trending = () => {
                         />
                       </div>
                       <div className="mt-6">
-                        <p className="text-sm text-gray-500">{product.color}</p>
+                        {/* <p className="text-sm text-gray-500">{product.color}</p> */}
                         <h3 className="mt-1 font-semibold text-gray-900">
-                          <a href={product.href}>
+                          <a href={`/products/${product.slug}`}>
                             <span className="absolute inset-0" />
                             {product.name}
                           </a>
@@ -76,7 +141,6 @@ const Trending = () => {
         </div>
       </div>
     </section>
-
   );
 };
 
